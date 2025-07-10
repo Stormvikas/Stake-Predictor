5931a80c3e64dc4eb9bfcd2bf28a70e831d2bc5e
Stormvikas <143207845+Stormvikas@users.noreply.github.com>
2025-07-11 02:11:56 +0530

    Create Verifier.js

---
+++ Verifier.js
@@ -0,0 +1,19 @@
+import { Alert } from 'react-native';
+
+export default function Verifier({ setServerSeed, setClientSeed, setNonce }) {
+  Alert.prompt(
+    'Load JSON',
+    'Paste your JSON: {"serverSeed":"...","clientSeed":"...","nonce":123}',
+    [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'OK',
+        onPress: jsonInput => {
+          try {
+            const data = JSON.parse(jsonInput);
+            if (data.serverSeed) setServerSeed(data.serverSeed);
+            if (data.clientSeed) setClientSeed(data.clientSeed);
+            if (data.nonce !== undefined) setNonce(data
